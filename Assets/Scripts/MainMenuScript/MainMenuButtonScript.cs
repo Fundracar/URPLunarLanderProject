da7865f62de42fad5b7ede81ecb5c0184cb69f61
Ryan Amarat <ryan.amarat@gmail.com>
2022-02-16 10:25:57 +0100

    Created MainMenuButton prefab and the MainMenuManger
    Clicking on a button makes it register itself to the canvas manager
    
    then, the canvas managers defines what to do.
    
    Commentaire : ca marche grave bien.

--- Assets/Scripts/MainMenuScript/MainMenuButtonScript.cs
+++ Assets/Scripts/MainMenuScript/MainMenuButtonScript.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class MainMenuButtonScript : MonoBehaviour
 {
+
+    private MainMenuCanvasManager mainMenuCanvasManagerRef;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        GameObject mainMenuCanvasReference = GameObject.FindGameObjectWithTag("MainMenuCanvas");
+
+        mainMenuCanvasManagerRef = mainMenuCanvasReference.GetComponent<MainMenuCanvasManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void OnClickRegistered()
+    {
+        Debug.Log("I was clicked");
+        mainMenuCanvasManagerRef.DefineClickedButton(this.gameObject);
     }
 }
--- Assets/Scripts/MainMenuScript/MainMenuCanvasManager.cs
+++ Assets/Scripts/MainMenuScript/MainMenuCanvasManager.cs
@@ -7,12 +7,46 @@ public class MainMenuCanvasManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void DefineClickedButton(GameObject _buttonClicked)
+
+    {
+
+        //This will define which button was clicked and trigger the appropriate behavior
+
+        string tagToCheck = _buttonClicked.tag;
+
+        switch (tagToCheck)
+        {
+            case "NewGameButton":
+                Debug.Log("NewGame Button was clicked");
+                break;
+
+            case "LoadGameButton":
+                Debug.Log("LoadGame Button was clicked");
+
+                break;
+
+            case "OptionsButton":
+                Debug.Log("OptionButton");
+                break;
+
+            case "QuitGameButton":
+                Debug.Log("QuitGameButton");
+                break;
+
+            default:
+                Debug.Log("Button Was Not Recognized");
+                break;
+        }
+
     }
 }
